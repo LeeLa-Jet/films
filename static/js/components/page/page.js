0f943587119a607e3250863829b98ec6cf22aec0
LeeLa-Jet <koggi@mail.ru>
2017-03-31 17:22:13 +0300

    Add page components

---
+++ static/js/components/page/page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Page extends Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+
+    )
+  }
+}
+
+export default Page;
